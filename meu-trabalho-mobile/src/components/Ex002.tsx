5817b6caab3612b1a983675890433f0bc013b721
Ana Caroline <155763806+ACPedrosa@users.noreply.github.com>
2025-07-14 01:34:23 +0000

    docs(Ex002): para o segundo exercicio do trabalho

---
+++ meu-trabalho-mobile/src/components/Ex002.tsx
@@ -0,0 +1,15 @@
+// src/components/Ex002.tsx
+
+import React from 'react'; //importação do React
+
+function Ex002() {
+    //declaração de variável
+    const nome = "Ana";
+    const anoNascimento = 2005;
+
+  return (
+    <p>Olá, meu nome é {nome} e nasci em {anoNascimento}.</p>
+  );
+}
+
+export default Ex002;
