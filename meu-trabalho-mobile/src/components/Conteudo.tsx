159a34a4e92956f8b42860451ff9287337c9112f
Ana Caroline <155763806+ACPedrosa@users.noreply.github.com>
2025-07-14 01:57:35 +0000

    docs(Conteudo.tsx): add resolução do exercicio 005 e 006

--- meu-trabalho-mobile/src/components/Conteudo.tsx
+++ meu-trabalho-mobile/src/components/Conteudo.tsx
@@ -2,13 +2,27 @@
 import '../App.css';
 
 function Conteudo() {
+    function mostrarAlerta(){
+        alert("Você clicou no botão!");
+    }
+    function executarAcao() {
+        alert("Função chamada com sucesso.");
+    }
+
   return (
     <div>
         <p>Este é o conteúdo principal.</p>
+
         {/* Botão com função onClick */}
-        <button onClick={() => alert("Você clicou no botão!")}>
+        <button onClick={mostrarAlerta}>
             Mostrar Alerta
         </button>
+
+        {/* Botão para função executarAcao no clique */}
+        <button onClick={executarAcao}>
+            Executar Ação
+        </button>
+        
     </div>
   );
 }
